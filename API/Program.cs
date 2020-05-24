b59abca4c8565d581013c13feca6cbf7bf534519
Frux <fruxsalomon@gmail.com>
2020-05-25 00:02:34 +0200

    Seeding data at runtime

--- API/Program.cs
+++ API/Program.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,16 +15,34 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var Logger =  services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                    await StorecontextSeed.SeedAsync(context, Logger);
+                }
+                catch (Exception ex)
+                {
+                  Logger.CreateLogger(ex.Message);
+                }
+            }
+
+            host.Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+         Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
     }
 }
--- Infrastructure/Data/StorecontextSeed.cs
+++ Infrastructure/Data/StorecontextSeed.cs
@@ -9,7 +9,7 @@ namespace Infrastructure.Data
         {
             if (!context.Products.Any())
             {
-                
+                    
             }
         }
     }
