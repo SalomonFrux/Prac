3004954f237ec2ec98681c18257afae976e09031
Frux <fruxsalomon@gmail.com>
2020-05-25 00:49:00 +0200

    Use your mind. Add in foreign tables first

--- Infrastructure/Data/StorecontextSeed.cs
+++ Infrastructure/Data/StorecontextSeed.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
@@ -7,10 +13,61 @@ namespace Infrastructure.Data
     {
         public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
         {
-            if (!context.Products.Any())
+
+            try
             {
-                    
+
+           
+             if (!context.ProductBrands.Any())
+            {
+                var ProductBrandsJson = File.ReadAllText("../Infrastructure/Data/DataSeed/brands.json");
+
+        var ProductBrandsJsonList =  JsonSerializer.Deserialize<List<ProductBrand>>(ProductBrandsJson);
+
+                foreach (var item in ProductBrandsJsonList)
+                {
+                    context.ProductBrands.Add(item);
+                }
+                await  context.SaveChangesAsync();
+             }
+
+            
+         if (!context.ProductTypes.Any())
+            {
+                var ProductTypesJson = File.ReadAllText("../Infrastructure/Data/DataSeed/types.json");
+                
+                var ProductTypesJsonList = 
+                JsonSerializer.Deserialize<List<ProductType>>(ProductTypesJson);
+
+                foreach (var item in ProductTypesJsonList)
+                {
+                    context.ProductTypes.Add(item);
+                }
+              await  context.SaveChangesAsync();
+             }
+
+             if (!context.Products.Any())
+            {
+                var productsJson = File.ReadAllText("../Infrastructure/Data/DataSeed/products.json");
+                var productsJsonList = JsonSerializer.Deserialize<List<Products>>(productsJson);
+
+                foreach (var item in productsJsonList)
+                {
+                    context.Products.Add(item);
+                }
+              await  context.SaveChangesAsync();
+             }
+
+
+               }
+            catch (Exception ex)
+            {
+                var logger =  loggerFactory.CreateLogger<StorecontextSeed>();
+                logger.LogError(ex.Message);
+               
             }
+
+            
         }
     }
 }
