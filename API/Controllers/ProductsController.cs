d007612dbfdc6f29936d6af7de443905114ab7f3
Frux <fruxsalomon@gmail.com>
2020-05-27 11:47:18 +0200

    Specification done

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -31,7 +31,8 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Products>>> GetProductsList()
         {
-            var products = await _genericProducts.GetEntitiesListSpec() //Create the specification class now
+            var spec = new ProductsWithBrandAndTypeSpec();
+            var products = await _genericProducts.GetEntitiesListSpec(spec); //Create the specification class now
             return Ok(products);
         }
 
